17f48a1277e7c7cc2bd47fe5d9923ba4b9ea6999
Can Beyhan <167461517+cnbyhn@users.noreply.github.com>
2024-11-09 13:06:28 +0300

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -17,27 +17,3 @@ class DataManager:
 
 data = DataManager()
 print(data.data)
-#response = requests.get("https://api.sheety.co/b4233df445ebd235db09302d7cfdcff0/flightDeals/prices")
-#flights = response.json()
-#print(flights)
-#for city in flights["prices"]:
-#    cities = city["city"]
-#    ids = city["id"]
-#    prices = city["lowestPrice"]
-#    params = {
-#      "keyword": cities
-#    }
-#    headers = {
-#      "Authorization": "Bearer iYU2Oz7S2jckYgBxJyelJKhsAs8n"
-#    }
-#    response1 = requests.get("https://test.api.amadeus.com/v1/reference-data/locations/cities", params=params,
-#                            headers=headers)
-#    code = response1.json()
-#    codes = code["data"][0]["iataCode"]
-#    data = {
-#      "price": {
-#      "iataCode" : codes
-#    }
-#    }
-#    putting = requests.put(f"https://api.sheety.co/b4233df445ebd235db09302d7cfdcff0/flightDeals/prices/{ids}",json=data)
-#    print(putting.text)
