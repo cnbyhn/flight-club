7f0910927e0e6df0dd797fd7ccf0dfd248fd3f15
Can Beyhan <167461517+cnbyhn@users.noreply.github.com>
2024-11-09 13:08:02 +0300

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -3,14 +3,14 @@ from twilio.rest import Client
 class NotificationManager:
     def __init__(self):
         self.OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-        self.api_key = "8e5c1f18afe5e44d22710ab81fac2a26"
-        self.account_sid = "ACe2c7a5cc583497f91455de58881ae7dc"
-        self.auth_token = "7c91c6db7a3011ca79a558f9071c8e40"
+        self.api_key = "your key"
+        self.account_sid = "your sid"
+        self.auth_token = "your token"
     def send_message(self,message):
         client = Client(self.account_sid, self.auth_token)
         message = client.messages.create(
             body= message,
-            from_="+12407135498",
-            to="+905389738050",
+            from_="+number",
+            to="+number",
         )
         print(message.status)
